e0a5e144f633acb8134957c4d9afbf36a6b9a5c4
yu <43624848+leey611@users.noreply.github.com>
2023-12-07 16:33:32 -0500

    add sound effects with tonejs

--- utils.js
+++ utils.js
@@ -32,3 +32,8 @@ export const useMousePosition = () => {
     }, []);
     return mousePosition;
 };
+
+export const Notes = [
+    "C5", "D5", "E5", "F5", "G5", "A5", "B5",
+    "C6", "D6", "E6", "F6", "G6", "A6", "B6",
+]
